a5bd238ea7a2fcd0c5e1198c1a0eed6f2556daa1
srod-85 <94401097+srod-85@users.noreply.github.com>
2022-02-23 13:07:17 -0800

    beginning of code

---
+++ WeeklyCodingChallenge1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace weeklyCodingChallenge1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string? str1;
+            int input;
+            int output;
+
+            do
+            {
+                Console.WriteLine("Enter an integer");
+                str1 = Console.ReadLine();
+            } while (str1 != null);
+        }
+
+    }
+}
