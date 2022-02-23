faa1bace02b6700b5818c508bc3d45b395ec2a57
srod-85 <94401097+srod-85@users.noreply.github.com>
2022-02-23 13:20:39 -0800

    created try-catch for input parsing, and added if-statement for the condition in which the number is outside of the range 1-9

--- WeeklyCodingChallenge1/Program.cs
+++ WeeklyCodingChallenge1/Program.cs
@@ -11,14 +11,26 @@ namespace weeklyCodingChallenge1
     {
         public static void Main(string[] args)
         {
-            string? str1;
-            int input;
-            int output;
-
+            string? str1 = "";
+            int input = 0;
+            int output = 0;
+            Console.WriteLine("Enter a number between 1 and 9");
             do
             {
-                Console.WriteLine("Enter an integer");
                 str1 = Console.ReadLine();
+                try
+                {
+                    input = int.Parse(str1);
+                }
+                catch(Exception)
+                {
+                    Console.WriteLine("Please enter a valid integer");
+                }
+                if(input < 1 || input > 9)
+                {
+                    Console.WriteLine("That number is out of range. Please enter a number between 1 and 9");
+                }
+
             } while (str1 != null);
         }
 
